5ddab670b6a99084fffaded71d79cd2b3743fd8b
Martin <martin.dinahet@proton.me>
2026-03-30 22:35:32 +0200

    feature: initial commit

---
+++ frontend/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { IndexPage } from "./pages/index-page";
+
+// biome-ignore lint/style/noNonNullAssertion: <>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <IndexPage />
+  </StrictMode>,
+);
